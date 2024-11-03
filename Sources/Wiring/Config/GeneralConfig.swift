7e191bbf6f9a192d3d07a09d6bc0cfcb1fa8f1a7
Benedek Kozma <cyberbeni@gmail.com>
2024-11-03 21:28:42 +0100

    Publish server state

--- Sources/Wiring/Config/GeneralConfig.swift
+++ Sources/Wiring/Config/GeneralConfig.swift
@@ -3,11 +3,14 @@ import Foundation
 extension Config {
 	struct General: Decodable {
 		let mqtt: Mqtt
+		private let _publishServerState: Bool?
+		var publishServerState: Bool { _publishServerState ?? true }
 		private let _enableDebugLogging: Bool?
 		var enableDebugLogging: Bool { _enableDebugLogging ?? false }
 
 		private enum CodingKeys: String, CodingKey {
 			case mqtt
+			case _publishServerState = "publishServerState"
 			case _enableDebugLogging = "enableDebugLogging"
 		}
 	}
