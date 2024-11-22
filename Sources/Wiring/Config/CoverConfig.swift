177febe2938147606024a08f728c6de660188b95
Benedek Kozma <cyberbeni@gmail.com>
2024-11-22 16:21:43 +0100

    Group some folders in the project

--- Sources/Wiring/Config/CoverConfig.swift
+++ Sources/Wiring/Config/CoverConfig.swift
@@ -2,15 +2,15 @@ import Foundation
 
 extension Config {
 	struct Cover: Decodable {
-        let remoteEntityId: String
-        let entries: [String: CoverItem]
+		let remoteEntityId: String
+		let entries: [String: CoverItem]
 
-        struct CoverItem: Decodable {
-            let remoteDevice: String
-            let openDuration: TimeInterval
-            let closeDuration: TimeInterval
-            let closedToOnePercentDuration: TimeInterval
-            let onePercentToClosedDuration: TimeInterval
-        }
-    }
+		struct CoverItem: Decodable {
+			let remoteDevice: String
+			let openDuration: TimeInterval
+			let closeDuration: TimeInterval
+			let closedToOnePercentDuration: TimeInterval
+			let onePercentToClosedDuration: TimeInterval
+		}
+	}
 }
