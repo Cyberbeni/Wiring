bf8ac5c565e3e9944eaeb22de3c9d50910584344
Benedek Kozma <cyberbeni@gmail.com>
2026-04-01 19:05:44 +0200

    Remove diacritics without Foundation

--- Sources/Wiring/Extensions/String+Extensions.swift
+++ Sources/Wiring/Extensions/String+Extensions.swift
@@ -1,9 +1,14 @@
-import Foundation
-
 extension String {
 	func toHomeAssistantAutodiscoveryTopic() -> String {
-		replacing(" ", with: "_")
-			.folding(options: .diacriticInsensitive, locale: .current)
+		String(
+			replacing(" ", with: "_")
+				// https://help.perforce.com/sourcepro/sposd/HTML/i18n/normalization_forms.htm
+				// NFKD
+				.decomposedStringWithCompatibilityMapping
+				.unicodeScalars
+				// https://en.wikipedia.org/wiki/Combining_Diacritical_Marks
+				.filter { $0.value < 0x300 || $0.value > 0x36F },
+		)
 	}
 
 	func toUniqueId() -> String {
