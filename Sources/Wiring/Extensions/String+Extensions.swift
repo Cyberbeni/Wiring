22f3bd48ef46425999321940fb7cf01ea8bdf416
Benedek Kozma <cyberbeni@gmail.com>
2026-04-01 19:17:10 +0200

    Use unicode scalar properties to decide if a character is a diacritic

--- Sources/Wiring/Extensions/String+Extensions.swift
+++ Sources/Wiring/Extensions/String+Extensions.swift
@@ -2,12 +2,9 @@ extension String {
 	func toHomeAssistantAutodiscoveryTopic() -> String {
 		String(
 			replacing(" ", with: "_")
-				// https://help.perforce.com/sourcepro/sposd/HTML/i18n/normalization_forms.htm
-				// NFKD
 				.decomposedStringWithCompatibilityMapping
 				.unicodeScalars
-				// https://en.wikipedia.org/wiki/Combining_Diacritical_Marks
-				.filter { $0.value < 0x300 || $0.value > 0x36F },
+				.filter { !$0.properties.isDiacritic },
 		)
 	}
 
