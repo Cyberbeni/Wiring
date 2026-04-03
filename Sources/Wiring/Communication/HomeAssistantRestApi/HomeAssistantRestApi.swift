4dc910f2c95834867e9a6b32bcd51cbcdaf3fd42
Benedek Kozma <cyberbeni@gmail.com>
2026-04-03 15:06:19 +0200

    Lower max response size

--- Sources/Wiring/Communication/HomeAssistantRestApi/HomeAssistantRestApi.swift
+++ Sources/Wiring/Communication/HomeAssistantRestApi/HomeAssistantRestApi.swift
@@ -1,15 +1,16 @@
 import AsyncHTTPClient
 
 nonisolated struct HomeAssistantRestApi {
-	static func jsonEncoder() -> JSONEncoder {
+	private static func jsonEncoder() -> JSONEncoder {
 		let encoder = JSONEncoder()
 		encoder.keyEncodingStrategy = .convertToSnakeCase
 		encoder.outputFormatting = [.sortedKeys, .withoutEscapingSlashes]
 		return encoder
 	}
 
-	let config: Config.HomeAssistant
-	let encoder = jsonEncoder()
+	private let config: Config.HomeAssistant
+	private let encoder = jsonEncoder()
+	private let maxResponseSize = 100_000
 
 	@concurrent
 	func callService(_ serviceCall: any HomeAssistantServiceCall) async {
@@ -31,7 +32,7 @@ nonisolated struct HomeAssistantRestApi {
 			if (200 ..< 300).contains(response.status.code) {
 				Log.debug("HTTP call OK.")
 			} else {
-				let responseData = try await response.body.collect(upTo: .max)
+				let responseData = try await response.body.collect(upTo: maxResponseSize)
 				let responseText = String(buffer: responseData)
 				Log.error("Error status code: \(response.status.code), body: \(responseText)")
 			}
